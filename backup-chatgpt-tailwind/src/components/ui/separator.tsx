669766810c188d08eca8160f0bbef1a9a38b16c4
Apolloluy <apollolouy@gmail.com>
2025-09-06 14:19:35 -0600

    chore: remove other modules

--- backup-chatgpt-tailwind/src/components/ui/separator.tsx
+++
@@ -1,5 +0,0 @@
-
-import * as React from 'react'
-export function Separator({ orientation='horizontal', className='' }:{orientation?:'horizontal'|'vertical', className?:string}){
-  return <div className={(orientation==='vertical'?'w-px h-6':'h-px w-full') + ' bg-slate-700/70 ' + className} />
-}
